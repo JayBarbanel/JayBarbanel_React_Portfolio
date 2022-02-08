c51c5f4c7fb7e549e200db8d96584c804493181e
Jay <jbarbanel531@gmail.com>
2022-02-08 14:02:08 -0800

    most recent, trying to fix bug

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
      <NavBar/>
 
     </div>
+
+    </Router>
   );
 }
 
