7080f677d4ce0f6bda4022fa6daef27a7a009b28
Jay <jbarbanel531@gmail.com>
2022-02-09 21:49:41 -0800

    finally got the carousel to work and the buttons in place.  need to make the image bigger but should be good to build new components

--- src/components/Commissioner.js
+++ src/components/Commissioner.js
@@ -2,27 +2,27 @@ import React from "react"
 import img from '../images/wz1.png'
 import img2 from '../images/wz2.png'
 import img3 from '../images/wz3.png'
-
+import "./Carousel.css"
 
 function Commissioner () {
     return (
         <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src={img}  alt="..."/>
+      <img src={img} class="w-20" alt="..."/>
     </div>
     <div class="carousel-item">
-      <img src={img2}  alt="..."/>
+      <img src={img2} class="w-20" alt="..."/>
     </div>
     <div class="carousel-item">
-      <img src={img3}  alt="..."/>
+      <img src={img3} class="w-20" alt="..."/>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+  <button id="pro" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+  <button id="pre"  type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
