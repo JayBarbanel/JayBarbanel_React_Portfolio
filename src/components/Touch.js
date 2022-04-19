bc516ff9d36559c676e85a1bed0f0c07db135d67
Jay <jbarbanel531@gmail.com>
2022-04-19 12:29:48 -0700

    updated styling on email button

--- src/components/Touch.js
+++ src/components/Touch.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import icon from '../Images/icon.jpg'
+import {
+    Button,
+}from 'react-bootstrap'
+
+import "./Warzone.css"
 
 
 function Touch () {
     return (
 <div>
-<p>Please feel free to reach out to me via email for any <br/> job oppertunities. </p>
- <img href="mailto:jbarbanel531@gmail.com" src={icon} class="w-10" />
+<p>Please feel free to reach out to me via email below for any <br/> job oppertunities. </p>
+ <Button className="button" href="mailto:jbarbanel531@gmail.com">
+ <img src={icon} class="w-10" />
+ </Button>
  </div>
     )
 }
